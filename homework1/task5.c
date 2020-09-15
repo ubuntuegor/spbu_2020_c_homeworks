f1eef53046a913522d60667ff4d70b48e05d3f91
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-15 21:31:27 +0300

    Homework 1, task 5

---
+++ homework1/task5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int comparison(const void* a, const void* b)
+{
+    return *(int*)b - *(int*)a;
+}
+
+int main()
+{
+    int numbers[] = { 1, 2, 3, 4, 3, 5, 1, 1, 2 };
+    int numbersLength = sizeof(numbers) / sizeof(int);
+
+    qsort(numbers, numbersLength, sizeof(int), comparison);
+
+    int lastNumber = numbers[0];
+    for (int i = 1; i < sizeof(numbers) / sizeof(int); i++) {
+        if (numbers[i] == lastNumber) {
+            printf("%d\n", numbers[i]);
+            break;
+        } else
+            lastNumber = numbers[i];
+    }
+
+    return 0;
+}
