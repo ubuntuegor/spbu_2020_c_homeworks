9540743c770e461c23d712cdc6b7caf8709f095e
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-22 20:46:29 +0300

    Use #pragma once as stated in WebKit style guide

--- library/commonUtils/numericOperations.h
+++ library/commonUtils/numericOperations.h
@@ -1,7 +1,4 @@
-#ifndef SPBU_2020_C_NUMERICOPERATIONS_H
-#define SPBU_2020_C_NUMERICOPERATIONS_H
+#pragma once
 
 int min(int a, int b);
 int max(int a, int b);
-
-#endif //SPBU_2020_C_NUMERICOPERATIONS_H
