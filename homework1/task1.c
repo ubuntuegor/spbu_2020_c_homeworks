2b710de5faa55ee78eac154878f5ceb96d1ca0ab
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-23 20:06:34 +0300

    Make it user-friendly

--- homework1/task1.c
+++ homework1/task1.c
@@ -6,26 +6,35 @@ typedef struct Point {
     int y;
 } Point;
 
-void printSquare(int** numbers, int size, Point* p)
+void printGrid(int** numbers, int size)
+{
+    for (int y = 0; y < size; y++) {
+        for (int x = 0; x < size; x++)
+            printf("%d\t", numbers[x][y]);
+        printf("\n");
+    }
+}
+
+void printEdge(int** numbers, int size, Point* p)
 {
     // prints values clockwise starting from top right
     for (int i = 0; i < size - 2; i++) {
-        printf("%d\n", numbers[p->x][p->y]);
+        printf("%d ", numbers[p->x][p->y]);
         p->y++;
     }
 
     for (int i = 0; i < size - 1; i++) {
-        printf("%d\n", numbers[p->x][p->y]);
+        printf("%d ", numbers[p->x][p->y]);
         p->x--;
     }
 
     for (int i = 0; i < size - 1; i++) {
-        printf("%d\n", numbers[p->x][p->y]);
+        printf("%d ", numbers[p->x][p->y]);
         p->y--;
     }
 
     for (int i = 0; i < size; i++) {
-        printf("%d\n", numbers[p->x][p->y]);
+        printf("%d ", numbers[p->x][p->y]);
         p->x++;
     }
 }
@@ -44,18 +53,23 @@ int main()
 
     int** numbers = (int**)malloc(n * sizeof(*numbers));
     // initialize grid with consecutive numbers
-    for (int i = 0; i < n; i++) {
-        numbers[i] = (int*)malloc(n * sizeof(**numbers));
-        for (int j = 0; j < n; j++)
-            numbers[i][j] = j * n + i + 1;
+    for (int x = 0; x < n; x++) {
+        numbers[x] = (int*)malloc(n * sizeof(**numbers));
+        for (int y = 0; y < n; y++)
+            numbers[x][y] = y * n + x + 1;
     }
 
+    printf("\nSource array:\n");
+    printGrid(numbers, n);
+
+    printf("\nOutput spiral:\n");
     int center = (n - 1) / 2;
     Point p = { center, center };
-    printf("%d\n", numbers[p.x][p.y]);
+    printf("%d ", numbers[p.x][p.y]);
     p.x++;
     for (int size = 3; size <= n; size += 2)
-        printSquare(numbers, size, &p);
+        printEdge(numbers, size, &p);
+    printf("\n");
 
     // free allocated memory
     for (int i = 0; i < n; i++)
