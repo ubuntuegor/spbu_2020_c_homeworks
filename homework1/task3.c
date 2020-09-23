8a4b58e69cf76ce7cdc545905c8b8bc4c8b30136
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-23 21:15:15 +0300

    Make it pretty and add string initialization

--- homework1/task3.c
+++ homework1/task3.c
@@ -2,17 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
+const int stringLimit = 1000;
+
 int main()
 {
-    char s1[100];
-    char s2[100];
+    char s1[stringLimit];
+    char s2[stringLimit];
 
-    printf("haystack: ");
-    fgets(s1, 100, stdin);
-    printf("needle: ");
-    fgets(s2, 100, stdin);
+    for (int i = 0; i < stringLimit; i++) { // Initialize strings because someone asked for that.
+        s1[i] = NULL;
+        s2[i] = NULL;
+    }
 
-    int s1Length = strlen(s1) - 1; // fgets includes newline at the end of the string
+    printf("Haystack: ");
+    fgets(s1, stringLimit, stdin);
+    printf("Needle: ");
+    fgets(s2, stringLimit, stdin);
+
+    int s1Length = strlen(s1) - 1; // "fgets" result includes a newline at the end so we account for that.
     int s2Length = strlen(s2) - 1;
     int searchRegion = s1Length - s2Length + 1;
 
@@ -32,6 +39,6 @@ int main()
             res++;
     }
 
-    printf("%d\n", res);
+    printf("Number of occurrences: %d\n", res);
     return 0;
 }
