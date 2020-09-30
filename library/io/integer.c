8b4a482d2d5026100c8c2794dd1c1afad164ba53
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 13:00:41 +0300

    Add printIntArray into library

--- library/io/integer.c
+++ library/io/integer.c
@@ -5,3 +5,10 @@ void promptInt(char prompt[], int* variablePtr)
     printf(prompt);
     scanf("%d", variablePtr);
 }
+
+void printIntArray(int array[], int length)
+{
+    for (int i = 0; i < length; i++)
+        printf("%d ", array[i]);
+    printf("\n");
+}
