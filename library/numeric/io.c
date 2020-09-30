40404983af2b50c453c420e343d896a92018e5ce
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 18:25:07 +0300

    Revert "numericIO: implement promptLLU"
    This reverts commit 6936d24d4676ceef198442ccbbf8de086977d188.

--- library/numeric/io.c
+++ library/numeric/io.c
@@ -7,12 +7,6 @@ void promptInt(char prompt[], int* variablePtr)
     scanf("%d", variablePtr);
 }
 
-void promptLLU(char prompt[], unsigned long long* variablePtr)
-{
-    printf(prompt);
-    scanf("%llu", variablePtr);
-}
-
 void promptIntArray(char prompt[], int array[], int length)
 {
     printf(prompt);
