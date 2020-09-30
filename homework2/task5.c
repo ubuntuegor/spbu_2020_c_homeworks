039da214f8edf336f6cc424d5d88fdcbcaf0f7c9
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 12:38:45 +0300

    Homework 2, task 5: refactor

--- homework2/task5.c
+++ homework2/task5.c
@@ -1,15 +1,16 @@
+#include "../library/io/integer.h"
 #include <stdio.h>
 
-int factorial(int n)
+long long factorialIterative(long long n)
 {
-    int result = 1;
+    long long result = 1;
     for (int i = 1; i <= n; i++) {
         result *= i;
     }
     return result;
 }
 
-int factorialRecursive(int n)
+long long factorialRecursive(long long n)
 {
     if (n <= 1)
         return 1;
@@ -18,7 +19,9 @@ int factorialRecursive(int n)
 
 int main()
 {
-    printf("Factorial 5: %d\n", factorial(5));
-    printf("Factorial (recursive) 5: %d\n", factorialRecursive(5));
+    int n = 0;
+    promptInt("n: ", &n);
+    printf("Factorial (iterative) %d: %lld\n", n, factorialIterative(n));
+    printf("Factorial (recursive) %d: %lld\n", n, factorialRecursive(n));
     return 0;
 }
