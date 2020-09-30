bb4c58c0ed19a1b53a7fd818b7d25d25d0400fee
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 13:10:43 +0300

    ioInteger: use possibly better increment

--- library/io/integer.c
+++ library/io/integer.c
@@ -8,7 +8,7 @@ void promptInt(char prompt[], int* variablePtr)
 
 void printIntArray(int array[], int length)
 {
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < length; ++i)
         printf("%d ", array[i]);
     printf("\n");
 }
