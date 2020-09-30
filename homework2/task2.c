8451b40c9841b57bb62c87042e50c6ec336545b9
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 18:17:25 +0300

    Homework 2, task 2: refactor

--- homework2/task2.c
+++ homework2/task2.c
@@ -1,105 +1,122 @@
+#include "../library/numeric/io.h"
 #include "../library/numeric/operations.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-void generateGameNumber(int* result, int length)
+void askForGameRules(int* digitCount)
 {
-    int i = 0;
-    while (i < length) {
-        result[i] = rand() / (double)RAND_MAX * 10;
-        if (result[i] == 10)
-            result[i]--;
-        bool unique = true;
-        for (int j = 0; j < i; j++) {
-            if (result[j] == result[i]) {
-                unique = false;
-                break;
-            }
-        }
-        if (unique)
-            i++;
+    bool correctInput = false;
+    while (!correctInput) {
+        promptInt("\nHow many digits should there be in my number? (4 to 10): ", digitCount);
+
+        if (*digitCount >= 4 && *digitCount <= 10)
+            correctInput = true;
+        else
+            printf("Please choose another number.\n");
     }
 }
 
-bool checkUnique(int* digits, int length)
+void generateGameNumber(int result[], int length)
 {
-    bool result = true;
-    for (int i = 1; i < length; i++) {
-        for (int j = 0; j < i; j++) {
-            if (digits[i] == digits[j]) {
-                result = false;
-                break;
+    int i = 0;
+    bool* usedDigits = (bool*)calloc(10, sizeof(bool));
+    while (i < length) {
+        int generated = rand() % 10;
+        if (!usedDigits[generated]) {
+            result[i] = generated;
+            usedDigits[generated] = true;
+            ++i;
+        }
+    }
+    free(usedDigits);
+}
+
+bool checkUnique(int digits[], int length)
+{
+    bool* usedDigits = (bool*)calloc(10, sizeof(bool));
+    for (int i = 0; i < length; ++i) {
+        int digit = digits[i];
+        if (usedDigits[digit]) {
+            free(usedDigits);
+            return false;
+        }
+        usedDigits[digit] = true;
+    }
+    free(usedDigits);
+    return true;
+}
+
+void countBullsAndCows(int correctDigits[], int guessedDigits[], int digitCount, int* bulls, int* cows)
+{
+    for (int i = 0; i < digitCount; ++i) {
+        bool foundPair = false;
+        for (int j = 0; j < digitCount && !foundPair; ++j) {
+            if (guessedDigits[i] == correctDigits[j]) {
+                if (i == j)
+                    ++(*bulls);
+                else
+                    ++(*cows);
+                foundPair = true;
             }
         }
-        if (!result)
-            break;
     }
-    return result;
 }
 
 int main()
 {
-    printf("Bulls and Cows\n\nI'm thinking of a 4-digit number... Try to guess it and I will "
-           "tell you how many digits of your number are in the right position (bulls) or "
-           "wrong position (cows).\nGood luck!\n");
+    printf("Bulls and Cows\n\nI'm thinking of a number... Try to guess it and I will tell you "
+           "how many digits of your number are in the right position (bulls) or wrong position "
+           "(cows).\nGood luck!\n");
     srand(time(NULL)); // Set random seed to current time.
 
-    int correctDigits[4] = { 0, 0, 0, 0 };
-    generateGameNumber(correctDigits, 4);
+    int digitCount = 0;
+    askForGameRules(&digitCount);
 
-    while (1) {
-        int guess = 0;
-        printf("\nGuess: ");
-        scanf("%d", &guess);
+    int* correctDigits = (int*)calloc(digitCount, sizeof(int));
+    generateGameNumber(correctDigits, digitCount);
 
-        int guessDigits[4] = { 0, 0, 0, 0 };
+    bool guessed = false;
+    while (!guessed) {
+        unsigned long long guess = 0;
+        promptLLU("\nYour guess: ", &guess);
 
         if (guess < 0) {
             printf("Negative number? Seriously? Read the game rules once again.\n");
             continue;
         }
 
-        if (guess > 9999) {
+        int guessDigitCount = countDigits(guess);
+        if (guessDigitCount > digitCount) {
             printf("Your number is too large!\n");
             continue;
         }
 
-        dissectNumberToDigits(guessDigits, 4, guess);
+        int* guessDigits = (int*)calloc(digitCount, sizeof(int));
+        dissectNumberToDigits(guessDigits, digitCount, guess);
 
-        if (!checkUnique(guessDigits, 4)) {
+        if (!checkUnique(guessDigits, digitCount)) {
             printf("Your number shouldn't contain repeating digits!\n");
+            free(guessDigits);
             continue;
         }
 
         int bulls = 0;
         int cows = 0;
 
-        // Check bulls
-        for (int i = 0; i < 4; i++)
-            if (guessDigits[i] == correctDigits[i])
-                bulls++;
+        countBullsAndCows(correctDigits, guessDigits, digitCount, &bulls, &cows);
 
-        if (bulls == 4) {
-            printf("You guessed it! Congratulations!\n");
-            break;
-        }
+        if (bulls == digitCount)
+            guessed = true;
+        else
+            printf("%d bulls, %d cows\n", bulls, cows);
 
-        // Check cows
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i == j)
-                    continue;
-                if (guessDigits[i] == correctDigits[j]) {
-                    cows++;
-                    break;
-                }
-            }
-        }
-
-        printf("%d bulls, %d cows\n", bulls, cows);
+        free(guessDigits);
     }
+
+    printf("You guessed it! Congratulations!\n");
+    free(correctDigits);
 
     return 0;
 }
