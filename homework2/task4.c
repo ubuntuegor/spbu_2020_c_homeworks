2a628acd5445f7d8a69f8c93cec593aca54c20b5
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 14:45:25 +0300

    Homework 2, task 4: fixes

--- homework2/task4.c
+++ homework2/task4.c
@@ -7,33 +7,46 @@ int comparison(const void* a, const void* b)
     return *(int*)a - *(int*)b;
 }
 
+void promptLLU(char prompt[], unsigned long long* variablePtr)
+{
+    printf(prompt);
+    scanf("%llu", variablePtr);
+}
+
+void printArrayAsNumber(int array[], int length)
+{
+    for (int i = 0; i < length; i++)
+        printf("%d", array[i]);
+    printf("\n");
+}
+
+void getMinimalNumberFromDigits(int digits[], int digitsLength)
+{
+    qsort(digits, digitsLength, sizeof(int), comparison);
+
+    int firstNonZero = 0;
+    while (firstNonZero < digitsLength && digits[firstNonZero] == 0)
+        firstNonZero++;
+    swap(&digits[0], &digits[firstNonZero]);
+}
+
 int main()
 {
-    int number = 0;
-    printf("Enter a number: ");
-    scanf("%d", &number);
+    unsigned long long number = 0;
+    promptLLU("Enter a number: ", &number);
 
     if (number < 0) {
         printf("Number must not be negative.\n");
         return 0;
     }
 
-    int digitsCount = countDigits(number);
-    int* digits = (int*)calloc(digitsCount, sizeof(int));
-    dissectNumberToDigits(digits, digitsCount, number);
-
-    qsort(digits, digitsCount, sizeof(int), comparison);
-
-    int firstNonZero = 0;
-    while (firstNonZero < digitsCount && digits[firstNonZero] == 0)
-        firstNonZero++;
-    swap(&digits[0], &digits[firstNonZero]);
+    int digitsLength = countDigits(number);
+    int* digits = (int*)calloc(digitsLength, sizeof(int));
+    dissectNumberToDigits(digits, digitsLength, number);
+    getMinimalNumberFromDigits(digits, digitsLength);
 
     printf("Result: ");
-    for (int i = 0; i < digitsCount; i++)
-        printf("%d", digits[i]);
-    printf("\n");
-
+    printArrayAsNumber(digits, digitsLength);
     free(digits);
 
     return 0;
