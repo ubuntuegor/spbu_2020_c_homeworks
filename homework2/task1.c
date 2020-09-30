61b4990c569513d7d41de471c15f504c97304143
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 13:26:24 +0300

    Homework 2, task 1: make memory management part of the solution

--- homework2/task1.c
+++ homework2/task1.c
@@ -1,10 +1,19 @@
 #include "../library/numeric/io.h"
 #include "../library/numeric/operations.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void printAllNumberDecompositions(int number, int limit, int prefix[], int prefixLength)
 {
+    // Allocate memory on first run.
+    bool haveToDeallocate = false;
+    if (prefix == NULL) {
+        prefixLength = 0;
+        prefix = (int*)calloc(number, sizeof(int));
+        haveToDeallocate = true;
+    }
+
     if (number == 0) {
         printIntArray(prefix, prefixLength);
         return;
@@ -14,15 +23,15 @@ void printAllNumberDecompositions(int number, int limit, int prefix[], int prefi
         prefix[prefixLength] = first;
         printAllNumberDecompositions(second, first, prefix, prefixLength + 1);
     }
+
+    if (haveToDeallocate)
+        free(prefix);
 }
 
 int main()
 {
     int n = 0;
     promptInt("n: ", &n);
-
-    int* prefix = (int*)calloc(n, sizeof(int));
-    printAllNumberDecompositions(n, n, prefix, 0);
-    free(prefix);
+    printAllNumberDecompositions(n, n, NULL, 0);
     return 0;
 }
