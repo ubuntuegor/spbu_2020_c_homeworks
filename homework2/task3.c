c61e8d2d4f63e94d2c329511a2679ce29d7f35e5
Egor Kukoverov <egorxxx29@gmail.com>
2020-09-30 13:55:55 +0300

    Homework 2, task 3: refactor

--- homework2/task3.c
+++ homework2/task3.c
@@ -1,36 +1,28 @@
+#include "../library/numeric/io.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     int n = 0;
-    printf("Number of input numbers: ");
-    scanf("%d", &n);
+    promptInt("Number of input numbers: ", &n);
 
     int* numbers = (int*)calloc(n, sizeof(int));
-    printf("Enter numbers:\n");
-    for (int i = 0; i < n; i++)
-        scanf("%d", &numbers[i]);
+    promptIntArray("Enter numbers:\n", numbers, n);
 
     int i = 0;
     int p = 0;
-
     while (i < n) {
-        while (p < n && numbers[p] == 0)
-            p++;
-        if (p < n)
-            numbers[i] = numbers[p];
-        else
-            numbers[i] = 0;
-        i++;
-        p++;
+        while (p < n && numbers[p] == 0) {
+            ++p;
+        }
+        numbers[i] = (p < n) ? numbers[p] : 0;
+        ++i;
+        ++p;
     }
 
     printf("Result: ");
-    for (int i = 0; i < n; i++)
-        printf("%d ", numbers[i]);
-    printf("\n");
-
+    printIntArray(numbers, n);
     free(numbers);
 
     return 0;
