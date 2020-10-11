257d7fccf9e0ff2f3dcb9f61853be3bc97799e51
Egor Kukoverov <egorxxx29@gmail.com>
2020-10-11 18:29:38 +0300

    Revert "numericList: implement cyclic list"
    This reverts commit f7e9cfc8542e489662eaef18464c41de280094fa.

--- library/numeric/list.h
+++ library/numeric/list.h
@@ -13,7 +13,6 @@ ListElementI* getHeadI(ListI*);
 ListElementI* getTailI(ListI*);
 int getLengthI(ListI*);
 ListElementI* getElementI(ListI*, int position);
-ListElementI* getNextElementI(ListElementI*);
 int getElementValueI(ListElementI*);
 void setElementValueI(ListElementI*, int value);
 bool insertElementI(ListI*, int position, ListElementI*);
@@ -24,7 +23,3 @@ void printListI(ListI*);
 
 bool removeElementI(ListI*, int position);
 bool destroyListI(ListI*);
-
-void makeCycleI(ListI*);
-void breakCycleI(ListI*);
-bool isCyclicI(ListI*);
