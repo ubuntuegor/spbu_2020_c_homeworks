0293308c1f05d7eaa78ea14869ecc7776eb9c464
Egor Kukoverov <egorxxx29@gmail.com>
2020-10-12 13:56:45 +0300

    Homework 3, task 2: adapt for library

--- homework3/task2.c
+++ homework3/task2.c
@@ -1,6 +1,18 @@
-#include "../library/numeric/cyclicList.h"
 #include "../library/numeric/io.h"
+#include "../library/numeric/list.h"
 #include <stdio.h>
+
+List* createWarriors(int number)
+{
+    List* warriors = createList();
+
+    for (int i = number; i > 0; --i) {
+        ListElement* warrior = createListElement(i);
+        insert(warriors, 0, warrior);
+    }
+
+    return warriors;
+}
 
 int main()
 {
@@ -14,19 +26,14 @@ int main()
         return 0;
     }
 
-    List* warriors = createList();
-
-    for (int i = n; i > 0; --i) {
-        ListElement* warrior = createListElement(i);
-        insertCyclic(warriors, 0, warrior);
-    }
+    List* warriors = createWarriors(n);
 
     ListElement* prevToKill = getLastElement(warriors);
     while (getLength(warriors) > 1) {
         for (int i = 0; i < m - 1; ++i)
-            prevToKill = getNextElement(prevToKill);
-        int toKillPosition = locate(warriors, getNextElement(prevToKill));
-        ejectCyclic(warriors, toKillPosition);
+            prevToKill = getNextElementInCycle(prevToKill, warriors);
+        int toKillPosition = locate(warriors, getNextElementInCycle(prevToKill, warriors));
+        eject(warriors, toKillPosition);
     }
 
     printf("Remaining warrior: %d\n", getElementValue(getFirstElement(warriors)));
