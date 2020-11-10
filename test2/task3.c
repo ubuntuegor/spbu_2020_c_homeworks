16842170941ea96cdf17c398f1c12ddd0bc430e2
Egor Kukoverov <egorxxx29@gmail.com>
2020-11-10 12:38:36 +0300

    Test 2, task 3

---
+++ test2/task3.c
@@ -0,0 +1,44 @@
+#include "../library/numeric/io.h"
+#include "../library/numeric/list.h"
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    promptInt("a: ", &a);
+    promptInt("b: ", &b);
+
+    FILE* filePointer = fopen("numbersab.txt", "r");
+
+    if (filePointer == NULL) {
+        printf("Make sure numbersab.txt is available in the working directory.\n");
+        return 0;
+    }
+
+    List* listA = createList();
+    List* listAB = createList();
+    List* listB = createList();
+
+    int d = 0;
+
+    while (!feof(filePointer)) {
+        fscanf(filePointer, "%d", &d);
+        ListElement* element = createListElement(d);
+        if (d < a) {
+            insert(listA, getLength(listA), element);
+        } else if (d >= a && d <= b) {
+            insert(listAB, getLength(listAB), element);
+        } else {
+            insert(listB, getLength(listB), element);
+        }
+    }
+
+    printList("Less than a: ", listA);
+    printList("Between a and b: ", listAB);
+    printList("Larger than b: ", listB);
+
+    fclose(filePointer);
+    return 0;
+}
