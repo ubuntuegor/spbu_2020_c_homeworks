e8d5a36b2c3843e1312669f4e9b20a8acd0b5ce0
Egor Kukoverov <egorxxx29@gmail.com>
2020-12-03 19:22:04 +0300

    Homework 8, task 1

---
+++ homework8/task1.c
@@ -0,0 +1,77 @@
+#include "../library/dfa/dfa.h"
+#include "../library/string/io.h"
+#include <stdlib.h>
+#include <string.h>
+
+bool isDigit(char character)
+{
+    return character >= '0' && character <= '9';
+}
+
+DFAState* createNumberModel(bool isFinal)
+{
+    DFAState* state = createDFAState(isFinal);
+    addTransition(state, state, DFADigit, 0);
+    return state;
+}
+
+DFAState* createSignedNumberModel(bool isFinal, DFAState** digitStatePtr)
+{
+    DFAState* initialState = createDFAState(false);
+    DFAState* signState = createDFAState(false);
+    DFAState* digitState = createNumberModel(isFinal);
+
+    addTransition(initialState, signState, DFACharacter, '+');
+    addTransition(initialState, signState, DFACharacter, '-');
+    addTransition(initialState, digitState, DFADigit, 0);
+    addTransition(signState, digitState, DFADigit, 0);
+
+    if (digitStatePtr != NULL)
+        *digitStatePtr = digitState;
+    return initialState;
+}
+
+DFA* createRealNumberDFA()
+{
+    DFAState* exponent = createSignedNumberModel(true, NULL);
+    DFAState* dotState = createDFAState(false);
+    DFAState* fractionState = createNumberModel(true);
+    DFAState* wholeNumberState = NULL;
+    DFAState* initialState = createSignedNumberModel(true, &wholeNumberState);
+
+    addTransition(wholeNumberState, exponent, DFACharacter, 'E');
+    addTransition(wholeNumberState, dotState, DFACharacter, '.');
+    addTransition(dotState, fractionState, DFADigit, 0);
+    addTransition(fractionState, exponent, DFACharacter, 'E');
+
+    DFA* dfa = createDFA(initialState);
+    return dfa;
+}
+
+int main()
+{
+    printf("Enter a real number: ");
+
+    char breakChars[] = { '\n' };
+    char* inputString = readStringUntil(breakChars, sizeof(breakChars));
+    int inputStringLength = strlen(inputString) - 1; // Last character is a newline, ignore it.
+
+    DFA* realNumberDFA = createRealNumberDFA();
+
+    for (int i = 0; i < inputStringLength; ++i) {
+        char character = inputString[i];
+        if (isDigit(character))
+            moveDFA(realNumberDFA, DFADigit, 0);
+        else
+            moveDFA(realNumberDFA, DFACharacter, character);
+    }
+
+    if (isInFinalState(realNumberDFA))
+        printf("That's a valid real number.\n");
+    else
+        printf("Entered string is not a real number.\n");
+
+    destroyDFA(&realNumberDFA);
+    free(inputString);
+    return 0;
+}
---
+++ library/dfa/dfa.h
@@ -0,0 +1,20 @@
+#ifndef SPBU_2020_C_DFA_H
+#define SPBU_2020_C_DFA_H
+#include <stdbool.h>
+
+typedef struct DFA DFA;
+typedef struct DFAState DFAState;
+enum DFATransitionType { DFADigit,
+    DFALetter,
+    DFACharacter };
+
+DFAState* createDFAState(bool isFinal);
+void addTransition(DFAState* fromState, DFAState* toState, enum DFATransitionType, char character);
+bool destroyDFAState(DFAState**);
+
+DFA* createDFA(DFAState* initialState);
+void moveDFA(DFA*, enum DFATransitionType, char character);
+bool isInFinalState(DFA*);
+bool destroyDFA(DFA**);
+
+#endif /* SPBU_2020_C_DFA_H */
